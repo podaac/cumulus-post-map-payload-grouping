8533c400c169ba4467d0eca252744dba8648286a
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2025-02-13 16:11:54 -0600

    update code to work with cnm formatting in step function

--- cumulus_post_map_payload_grouping/cumulus_post_map_payload_grouping.py
+++ cumulus_post_map_payload_grouping/cumulus_post_map_payload_grouping.py
@@ -1,14 +1,16 @@
 def lambda_handler(event, context):
     fail_count = 0
-    for e in event:
-        print(e.get('Status'))
-        if e.get('Status') == 'FAILED':
+    for e in event.get('cma').get('event'):
+        cumulus_meta = e.get('cumulus_meta')
+        print(e.get('result'))
+        if e.get('result') != 'success':
             fail_count = fail_count + 1
 
     if fail_count > 0:
         raise Exception(f'Map has counted failed unpacks (total: {fail_count})')
 
     return {
+        'cumulus_meta': cumulus_meta,
         'statusCode': 200,
         'body': {
             "total_count": len(event),
